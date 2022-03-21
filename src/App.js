9435be19937a8c85dec284c17ef3de8826c35947
Lee-ye-ji <dpwl30559@gmail.com>
2022-03-21 17:41:52 +0900

    Chore: styled-component / redux / react-router-dom 설치 (#3)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import theme from './styles/theme';
+import GlobalStyle from './styles/global';
+import KartPage from './pages/KartPage';
+import SearchPage from './pages/SearchPage';
+import RankingPage from './pages/RankingPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>하이</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path="/" element={<KartPage />}>
+            <Route path="rank" element={<RankingPage />} />
+            <Route path="user:id" element={<SearchPage />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
